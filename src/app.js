7001b57a371de2c435e380b30902542cd12f53a2
amanxrawat <amanxrawat.93@gmail.com>
2024-08-29 19:12:03 +0530

    created user route and register the user

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express from 'express';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 
+const app = express();
+
 app.use(cors({
     origin:process.env.CORS_ORIGIN,
     credentials:true,
@@ -13,8 +15,13 @@ app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+// routes
+
+import userRouter from './routes/user.router.js'
+
+// routes declaration
+app.use('/api/v1/users',userRouter)
 
 
-const app = express();
 
 export {app}
